9785518c4e8ff5da13c02c544398539bccf91086
Kiet Pham <kietpham.dev@gmail.com>
2026-01-01 14:21:31 +1100

    create shared product data lib

---
+++ libs/shared/product/data/README.md
@@ -0,0 +1,7 @@
+# shared-product-data
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build shared-product-data` to build the library.
---
+++ libs/shared/product/data/src/lib/product-data.mock.ts
@@ -0,0 +1,14 @@
+import { Product } from '@nx-example/shared/product/types';
+
+export const mockProducts: Product[] = [
+  {
+    id: '0',
+    name: 'Product 1',
+    price: 10,
+  },
+  {
+    id: '1',
+    name: 'Product 2',
+    price: 20,
+  },
+];
