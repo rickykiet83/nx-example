c942d702cb465e1efeebb0ba182b6b893bcb6153
Kiet Pham <kietpham.dev@gmail.com>
2026-01-02 22:54:14 +1100

    remove shared jxs lib

--- libs/shared/jsxify/README.md
+++
@@ -1,7 +0,0 @@
-# shared-jsxify
-
-This library was generated with [Nx](https://nx.dev).
-
-## Building
-
-Run `nx build shared-jsxify` to build the library.
--- libs/shared/jsxify/src/lib/shared-jsxify.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export type JSXify<T extends HTMLElement> =
-  React.DetailedHTMLProps<React.HTMLAttributes<T>, T> &
-  Partial<Record<keyof T, unknown>> & {
-    ref?: React.Ref<T>;
-  };
