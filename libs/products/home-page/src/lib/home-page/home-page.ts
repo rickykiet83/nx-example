725ae591c510c5a8b076d4da0236bfe41fcedf68
Kiet Pham <kietpham.dev@gmail.com>
2026-01-05 20:10:58 +1100

    implement add product to cart in product home page with inject service

--- libs/products/home-page/src/lib/home-page/home-page.ts
+++ libs/products/home-page/src/lib/home-page/home-page.ts
@@ -4,8 +4,11 @@ import { AsyncPipe, CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { ProductsState, loadProductsFromMock, productsActions, productsFeature } from '@nx-example/shared/product/state';
 
+import { Product } from '@nx-example/shared/product/types';
 import { RouterLink } from '@angular/router';
+import { StorageService } from '@nx-example/shared/product/service';
 import { Store } from '@ngrx/store';
+import { saveCartToStorage } from '@nx-example/shared/cart/services';
 
 @Component({
   selector: 'products-home-page',
@@ -17,9 +20,10 @@ import { Store } from '@ngrx/store';
 })
 export class HomePageComponent implements OnInit {
   private store = inject<Store<ProductsState>>(Store);
-
+  private storageService = inject(StorageService);
   products$ = this.store.select(productsFeature.selectProducts);
   status$ = this.store.select(productsFeature.selectStatus);
+
 
   ngOnInit(): void {
     // dispatch load
@@ -27,4 +31,6 @@ export class HomePageComponent implements OnInit {
     const products = loadProductsFromMock();
     this.store.dispatch(productsActions.loadSuccess({ products }));
   }
+
+  addProductToCart = (product: Product) => this.storageService.saveCartToStorage(product);
 }
--- libs/shared/product/services/src/lib/services/storage/storage.service.ts
+++ libs/shared/product/services/src/lib/services/storage/storage.service.ts
@@ -1,14 +1,18 @@
 import { Injectable, inject } from '@angular/core';
 
+import { Product } from '@nx-example/shared/product/types';
 import { Router } from '@angular/router';
 import { StorageServiceCore } from '@nx-example/web-core';
+import { saveCartToStorage } from '@nx-example/shared/cart/services';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
   private core = new StorageServiceCore();
-  private router = inject(Router)
+  private router = inject(Router);
+
+  saveCartToStorage = (product: Product, quantity = 1) => saveCartToStorage(product, quantity);
 
   getLocalItem(key: string) {
     return this.core.getLocalItem(key);
